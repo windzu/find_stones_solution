31f55550647143b5a591104e134efa286c15f068
windzu <windzu1@gmail.com>
2022-10-13 08:33:08 +0000

    Deploying to gh-pages from  @ 1d7945135b972c4b92a72a072bbc1b48306b4b0e 🚀

---
+++ CMakeFiles/unit_test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/runner/work/find_stones_solution/find_stones_solution/test/test.cpp" "test/CMakeFiles/unit_test.dir/test.cpp.o" "gcc" "test/CMakeFiles/unit_test.dir/test.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/runner/work/find_stones_solution/find_stones_solution/build/third_party/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/home/runner/work/find_stones_solution/find_stones_solution/build/third_party/googletest/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/home/runner/work/find_stones_solution/find_stones_solution/build/lib/CMakeFiles/solution.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/runner/work/find_stones_solution/find_stones_solution/test
+# Build directory: /home/runner/work/find_stones_solution/find_stones_solution/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_test "/home/runner/work/find_stones_solution/find_stones_solution/build/test/unit_test")
+set_tests_properties(unit_test PROPERTIES  _BACKTRACE_TRIPLES "/home/runner/work/find_stones_solution/find_stones_solution/test/CMakeLists.txt;8;add_test;/home/runner/work/find_stones_solution/find_stones_solution/test/CMakeLists.txt;0;")
